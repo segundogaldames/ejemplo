6db587c533316c29f34d3cc5d1bca4b22a29cbf5
Segundo <segundogaldames@gmail.com>
2021-12-04 15:38:54 -0300

    Carpeta application

---
+++ application/Config.php
@@ -0,0 +1,15 @@
+<?php
+
+define('BASE_URL', 'http://localhost:8080/veterinaria/');
+define('DEFAULT_CONTROLLER', 'index');
+define('DEFAULT_LAYOUT', 'default');
+
+define('SESSION_TIME', 10);
+define('HASH_KEY', '6186e10b88e85h186e120edd39');
+define('CTRL', '6186e13b048f46186e148b623e');
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS', '1234');
+define('DB_NAME', 'veterinaria');
+define('DB_CHAR', 'utf8');
